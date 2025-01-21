89680966bab15a26101f8942bdc65fe6b5a8fe61
Anthony Chablov <achablov@myseneca.ca>
2025-01-21 11:31:15 -0500

    feat: dashboard page route and footer component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/Layout/Container";
 import Hero from "./_components/Hero";
 import SuggestionDisplay from "./_components/SuggestionDisplay/SuggestionsDisplay";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Container>
         <SuggestionDisplay />
+        <Footer />
       </Container>
     </>
   );

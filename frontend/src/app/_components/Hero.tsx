476946fad39afc2d557991b4967c1604e6859a20
Anthony Chablov <achablov@myseneca.ca>
2025-01-21 12:48:41 -0500

    feat: add container to hero

--- frontend/src/app/_components/Hero.tsx
+++ frontend/src/app/_components/Hero.tsx
@@ -3,23 +3,26 @@ import HeaderText from "@/components/Text/HeaderText";
 import ParagraphText from "@/components/Text/ParagraphText";
 import QueryInputForm from "./QueryInputForm";
 import Separator from "@/components/Layout/Separator";
+import Container from "@/components/Layout/Container";
 
 const Hero = () => {
   return (
-    <section className="flex items-center justify-center flex-col h-screen bg-customGray text-center">
-      <HeaderText
-        className=""
-        headerLevel="h1"
-        header="Everything about wine"
-      />
-      <Separator className="" size="extraSmall" />
-      <ParagraphText
-        size="2xl"
-        className=" text-gray-500 text-"
-        text="What would you like to know?"
-      />
-      <Separator className="" size="small" />
-      <QueryInputForm />
+    <section className="flex items-center  justify-center flex-col h-screen bg-customGray text-center">
+      <Container>
+        <HeaderText
+          className=""
+          headerLevel="h1"
+          header="Everything about wine"
+        />
+        <Separator className="" size="extraSmall" />
+        <ParagraphText
+          size="2xl"
+          className=" text-gray-500 text-"
+          text="What would you like to know?"
+        />
+        <Separator className="" size="small" />
+        <QueryInputForm />
+      </Container>
     </section>
   );
 };

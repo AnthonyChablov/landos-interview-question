d9339a075e46e12af258e555f1e8dbf6a67d473e
Anthony Chablov <achablov@myseneca.ca>
2025-01-23 21:06:35 -0500

    fix: make nav z-index higher

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -54,7 +54,7 @@ const Nav = () => {
   };
 
   return (
-    <div className="sticky top-0 left-0 right-0 z-10 bg-white py-6 shadow-sm">
+    <div className="sticky  top-0 left-0 right-0 z-30 bg-white py-6 shadow-sm">
       <Container className="flex items-center justify-between">
         {/* Logo */}
         <div className="text-xl font-bold">

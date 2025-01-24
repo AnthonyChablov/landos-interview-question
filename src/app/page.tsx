37cd278c6427374a6ce94adeaafb418e37d350b6
Anthony Chablov <achablov@myseneca.ca>
2025-01-23 20:44:16 -0500

    feat: add smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ChatInterface from "./_components/ChatInterface/ChatInterface";
+import useLenisSmoothScroll from "@/hooks/useLenisSmoothScroll";
 
 /**
  * Page Component
  * Renders the main page with a chat interface
  */
 const page = () => {
+  useLenisSmoothScroll();
   return (
     <>
       <ChatInterface />

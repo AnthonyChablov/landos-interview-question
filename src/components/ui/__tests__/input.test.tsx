851ea1e899a9a01ef53b190594236c2a302dcaf1
Anthony Chablov <achablov@myseneca.ca>
2025-01-26 13:29:52 -0500

    feat/refactor: reduce placeholder width and input tests refactor

--- src/components/ui/__tests__/input.test.tsx
+++ src/components/ui/__tests__/input.test.tsx
@@ -47,14 +47,4 @@ describe("Input Component", () => {
     const input = screen.getByPlaceholderText("Disabled Input");
     expect(input).toBeDisabled();
   });
-
-  it("should call the cn utility with the correct class names", () => {
-    render(<Input className="additional-class" placeholder="Input with cn" />);
-
-    // Ensure that the cn function is called with the correct arguments and applied class names
-    expect(cn).toHaveBeenCalledWith(
-      "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground truncate focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-      "additional-class"
-    );
-  });
 });
